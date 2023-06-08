496fcab48e76df3d5361767690d93e41f36773b3
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-08 09:09:50 +0530

    created responsive navs

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,18 @@
-import logo from "../../public/hill_queen-1.png"
+import logo from "../hill_queen-1.png"
 import {motion} from "framer-motion"
 import { useState } from "react"
 import useMediaQuery from "../util/useMediaQuery"
 export default function Nav()
 {
     const[toggled,setToggled] = useState(false)
-    const matches=useMediaQuery('(min-width:128px)')
+    const matches=useMediaQuery("(min-width:1280px)")
     console.log(matches)
     return (
-        <nav className="relative mx-12 mb-12 flex justify-between items-center pt-8  pb-0 font-medium md:mx-21 lg:mx-32 ">
+        <nav className="relative mx-8 mb-24 flex justify-between items-center pt-8  pb-0 font-medium md:mx-21 lg:mx-32 ">
             <svg 
-                className="absolute bottom-0 left-1/2 -translate-x-1/3"
+                className="absolute bottom-0 left-1/2 -translate-x-1/2"
                 width="250" 
-                height={6} 
+                height={4} 
                 viewBox="0 0 250 4" 
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -24,15 +24,20 @@ export default function Nav()
                     </div>
 
                     {/*Title*/}
+                     
                     <h1 className="text-lg font-bold">
                     <a href="/">Hill Queen</a>
                     </h1>
+                    
+                    {!matches &&(
 
-                    <div onClick={()=>setToggled(prevToggle=>!prevToggle)} className="space-y-1 coursor-pointer xl:hidden">
+
+                    <div onClick={()=>setToggled(prevToggle=>!prevToggle)} className="space-y-1 coursor-pointer">
                         <span className="block h-0.5 w-8 bg-black"></span>
                         <span className="block h-0.5 w-6 bg-black"></span>
                         <span className="block h-0.5 w-4 bg-black"></span>
                     </div>
+                    )}
         </nav>
     )
 
