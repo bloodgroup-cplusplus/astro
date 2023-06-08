d18d6f6be21ffee8883044fff7ecc31623545211
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-08 07:28:17 +0530

    navbar section

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,27 @@
-
+import {motion} from "framer-motion"
+import { useState } from "react"
 export default function Nav()
 {
-
+    const[toggled,setToggled] = useState(false)
     return (
-        <nav>
-            Logo
+        <nav className="relative mx-8 mb-24 ">
+            <svg 
+                width="250" 
+                height={4} 
+                viewBox="0 0 250 4" 
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M2 2L428 2" strokeWidth={2} stroke="#282828" strokeLinecap="round"/>
+                    </svg>
+                    <div>
+                        <img src="" alt=" profile picture of hua"/>
+                    </div>
+
+                    {/*Title*/}
+                    <h1 className="text-lg font-bold">
+                    <a href="/">Hua.</a>
+                    </h1>
         </nav>
     )
 
