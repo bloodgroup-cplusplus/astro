5d6ad586b8e5f1b3a4156eb53f61be34c7b06055
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-08 11:06:14 +0530

    added bunch of stuff

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,27 @@ import logo from "../hill_queen-1.png"
 import {motion} from "framer-motion"
 import { useState } from "react"
 import useMediaQuery from "../util/useMediaQuery"
+
+const navMotion = {
+    visible:{
+        opacity:1,
+        transition:{
+            when:"beforeChildren",
+            staggerChildren:0.15,
+        },
+    },
+    hidden:{
+        opacity:0,
+    },
+}
+const itemMotion={
+    visible:{opacity:1,x:0},
+    hidden:{opacity:0,x:-100},
+}
 export default function Nav()
 {
     const[toggled,setToggled] = useState(false)
     const matches=useMediaQuery("(min-width:1280px)")
-    console.log(matches)
     return (
         <nav className="relative mx-8 mb-24 flex justify-between items-center pt-8  pb-0 font-medium md:mx-21 lg:mx-32 ">
             <svg 
@@ -62,11 +78,14 @@ export default function Nav()
                         animate={{opacity:1,x:0}}
                         initial={{opacity:0,x:25}}
                         className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center">
-                        <div className="flex flex-col gap-24 text-lg ">
-                        <a href="/">Home</a>
-                        <a href="/tours">Tours</a>
-                        <a href="/contact">Contact</a>
-                        </div>
+                        <motion.div variants={navMotion}
+                         animate="visible"
+                         initial="hidden"
+                         className="flex flex-col gap-24 text-lg ">
+                        <motion.a variants={itemMotion} href="/">Home</motion.a>
+                        <motion.a variants={itemMotion} href="/tours">Tours</motion.a>
+                        <motion.a variants={itemMotion} href="/contact">Contact</motion.a>
+                        </motion.div>
                     </motion.div>
                     )}
         </nav>
